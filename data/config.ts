1655ae90928979703d8ca3c3dd18f40de088baec
SssSxxS <sssx0sxxx@gmail.com>
2025-02-23 11:57:07 +0300

    +FAUCET_ATTEMPTS

--- data/config.ts
+++ data/config.ts
@@ -3,6 +3,7 @@ export const SOMNIA_TESTNET_EXPLORER_URL = 'https://somnia-testnet.socialscan.io
 
 export const SHUFFLE_WALLETS = true // true/false shuffle wallets before use
 
+export const FAUCET_ATTEMPTS = 5
 export const FAUCET_SLEEP_RANGE = [100, 300] // [min, max] in seconds
 
 export const RANDOM_SEND_AMOUNT = [0.001, 0.005, 0.01] // 0.001 STT, 0.005 STT, 0.01 STT, you can add more options
--- src/commands/cmdGetSttFromFaucet.ts
+++ src/commands/cmdGetSttFromFaucet.ts
@@ -2,7 +2,7 @@ import { getDefaultHeaders, type FetchOptions } from '@/lib/defaultFetch'
 import { logger } from '@/lib/logger'
 import { getRandomInt, parseProxy, shuffleArray } from '@/lib/utils'
 import { getWalletsData } from '@/modules/xlsx'
-import { FAUCET_SLEEP_RANGE, SHUFFLE_WALLETS, SOMNIA_TESTNET_EXPLORER_URL } from '@data/config'
+import { FAUCET_ATTEMPTS, FAUCET_SLEEP_RANGE, SHUFFLE_WALLETS, SOMNIA_TESTNET_EXPLORER_URL } from '@data/config'
 import { HttpsProxyAgent } from 'https-proxy-agent'
 
 export const cmdGetSttFromFaucet = async () => {
@@ -31,14 +31,23 @@ export const cmdGetSttFromFaucet = async () => {
           }
         }
         logger.debug(`(${wallet.id}) Fetch options:%o`, fetchOptions)
-        const response = await fetch(url, fetchOptions)
-        logger.info(`(${wallet.id}) Sent request to faucet${wallet.proxy ? ` through ${wallet.proxy} proxy` : ''}`)
-        const responseData = await response.json()
-        if (!response.ok) {
-          logger.warn(`(${wallet.id}) Response is not ok: ${response.status} ${response.statusText}\n%o`, responseData)
-        } else {
-          const link = `${SOMNIA_TESTNET_EXPLORER_URL}/tx/${responseData.data.hash}}`
-          logger.success(`(${wallet.id}) Tokens should be received ${link}`)
+        for (let i = 0; i < FAUCET_ATTEMPTS; i++) {
+          const response = await fetch(url, fetchOptions)
+          logger.info(`(${wallet.id}) Sent request to faucet${wallet.proxy ? ` through ${wallet.proxy} proxy` : ''}`)
+          const responseData = await response.json()
+          if (!response.ok) {
+            logger.warn(
+              `(${wallet.id}) Response is not ok: ${response.status} ${response.statusText}\n%o`,
+              responseData
+            )
+            logger.info(`(${wallet.id}) Retrying...`)
+            await Bun.sleep(3000)
+            continue
+          } else {
+            const link = `${SOMNIA_TESTNET_EXPLORER_URL}/tx/${responseData.data.hash}}`
+            logger.success(`(${wallet.id}) Tokens should be received ${link}`)
+            break
+          }
         }
 
         if (wallet == wallets[wallets.length - 1]) break
